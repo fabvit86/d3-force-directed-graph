b23216f3815fd3042305c48a21f33afd0e6a2974
Fabrizio Vitelli <fabvit86@yahoo.it>
2017-09-25 13:50:48 +0200

    fixed nodes and links going outside svg borders

--- app/js/graph.js
+++ app/js/graph.js
@@ -9,12 +9,12 @@ const svg = d3.select("#svgchart")
 svg.attr("height", chartHeight)
 	 .attr("width", chartWidth)
 
-// functions to avoid nodes going outside the svg container, when calculating the position:
+// functions to avoid nodes and links going outside the svg container, when calculating the position:
 function getNodeXCoordinate(x) {
-	return Math.max(nodeWidth, Math.min(chartWidth - nodeWidth, x))
+	return Math.max(0, Math.min(chartWidth - nodeWidth, x))
 }
 function getNodeYCoordinate(y) {
-	return Math.max(nodeHeight, Math.min(chartHeight - nodeHeight, y))
+	return Math.max(0, Math.min(chartHeight - nodeHeight, y))
 }
 
 // create a new simulation (a simulation starts with alpha = 1 and decrese it slowly to 0):
@@ -112,12 +112,12 @@ d3.json(url, (error, data) => {
   	.nodes(nodes)
   	.on("tick", () => {
   		// set each node's position on each tick of the simulation:
-  		nodeWrapper.attr("transform", d => "translate(" + getNodeXCoordinate(d.x) + "," + getNodeXCoordinate(d.y) + ")")
+  		nodeWrapper.attr("transform", d => "translate(" + getNodeXCoordinate(d.x) + "," + getNodeYCoordinate(d.y) + ")")
   		// set start (x1,y1) and point (x2,y2) coordinate of each link on each tick of the simulation:
-  		link.attr("x1", d => d.source.x + nodeWidth / 2)
-  		link.attr("y1", d => d.source.y + nodeHeight / 2)
-  		link.attr("x2", d => d.target.x + nodeWidth / 2)
-  		link.attr("y2", d => d.target.y + nodeHeight / 2)
+  		link.attr("x1", d => getNodeXCoordinate(d.source.x + nodeWidth / 2))
+  		link.attr("y1", d => getNodeYCoordinate(d.source.y + nodeHeight / 2))
+  		link.attr("x2", d => getNodeXCoordinate(d.target.x + nodeWidth / 2))
+  		link.attr("y2", d => getNodeYCoordinate(d.target.y + nodeHeight / 2))
   	})
 
   // pass the links to the link force:
